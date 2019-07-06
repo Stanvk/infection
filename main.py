88f15ead63862f57c25eabca4b40c774ecd9d3a1
stanvk <stanvankraaij@hotmail.com>
2019-07-06 22:04:53 +0200

    Added initial random infection and some other features

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 import pygame
 import random
-import time
 
 # Define some colors
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 BLUE = [(51, 51, 255), (0, 0, 255), (0, 0, 204), (0, 0, 153), (0, 0, 102)] # Five different blues, from light to dark.
 RED = [(255, 51, 51), (255, 0, 0), (204, 0, 0), (153, 0, 0), (102, 0, 0)] # Five different reds, from light to dark.
 
@@ -15,7 +13,7 @@ WIDTH = 10
 HEIGHT = 10
 MARGIN = 1
 
-numberOfRows = 70
+numberOfRows = 71
 numberOfColumns = numberOfRows
 
 windowsWidth = (MARGIN+WIDTH)*numberOfColumns
@@ -40,10 +38,8 @@ for row in range(numberOfRows):
         ageOfCells[row].append(0)  # Append a cell
 
 
-
-
-
-
+#Blue is 0
+#Red is 1
 
 # Initialize pygame
 pygame.init()
@@ -53,7 +49,7 @@ WINDOW_SIZE = [windowsWidth, windowsHeight]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
-pygame.display.set_caption("Random Infection")
+pygame.display.set_caption("Random Infection RED vs BLUE - by Stanvk")
  
 # Loop until the user clicks the close button.
 done = False
@@ -61,32 +57,28 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-def determineColorAge(age):
-    if (age<1):
-        return 0
-    elif(age<10):
-        return 1
-    elif(age<40):
-        return 2
-    elif(age<80):
-        return 3
-    elif(age<120):
-        return 4
+#Initial infection of the grid
+for row in range(numberOfRows):
+    for column in range(round(numberOfColumns)):
+        if random.random() <= 0.5:
+            grid[row][column] = 1
+
+#Set the age of all cells at 121
+for row in range(numberOfRows):
+    for column in range(numberOfColumns):
+        ageOfCells[row][column] = 121
+
+#Count the neigbouring cells of the same type for a given cell.
+def countNeighboursOfSameType(row, column, type):
+    neighbours = [grid[row+1][column], grid[row-1][column], grid[row][column+1], grid[row][column-1], grid[row+1][column+1], grid[row-1][column-1]]
+
+    return neighbours.count(type)
 
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            # Change the x/y screen coordinates to grid coordinates
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-            # Set that location to one
-            grid[row][column] = 1
-            print("Click ", pos, "Grid coordinates: ", row, column)
 
  
     # Set the screen background
@@ -94,23 +86,59 @@ while not done:
 
     for row in range(numberOfRows):
         for column in range(numberOfColumns):
-            #Cell gets older..
-            ageOfCells[row][column] = ageOfCells[row][column] + 1
 
-            #Infect
+            statusBefore = grid[row][column]
+
+
             if row > 0 and column > 0 and row < (numberOfRows-1) and column < (numberOfColumns-1):
-                #From white to red infection due neighbour red cell
-                if grid[row+1][column] == 1 or grid[row-1][column] == 1 or grid[row][column+1] == 1 or grid[row][column-1] == 1:
-                    if random.random() <= 0.05:
+
+                #Infect
+                if countNeighboursOfSameType(row, column, 1) != 0:
+                    chanceOfInfection = 0.06*countNeighboursOfSameType(row, column, 1)
+                    if random.random() <= chanceOfInfection:
                         grid[row][column] = 1
+
+                #disinfect
+                if countNeighboursOfSameType(row, column, 0) != 0:
+                    chanceOfDisinfection = 0.05*countNeighboursOfSameType(row, column, 0);
+                    if random.random() <= chanceOfDisinfection:
+                        grid[row][column] = 0
+
+            if statusBefore == grid[row][column]:
+                ageOfCells[row][column] = ageOfCells[row][column] + 1
+            elif statusBefore != grid[row][column]:
+                ageOfCells[row][column] = 0
+
                 
 
     # Draw the grid
     for row in range(numberOfRows):
         for column in range(numberOfRows):
-            # color = WHITE
+
+            if grid[row][column] == 0:
+                color = BLUE[0]
+                if ageOfCells[row][column] < 30:
+                    color = BLUE[1]
+                elif ageOfCells[row][column] < 60:
+                    color = BLUE[2]
+                elif ageOfCells[row][column] < 90:
+                    color = BLUE[3]
+                elif ageOfCells[row][column] > 120:
+                    color = BLUE[4]
+
             if grid[row][column] == 1:
                 color = RED[0]
+                if ageOfCells[row][column] < 30:
+                    color = RED[1]
+                elif ageOfCells[row][column] < 60:
+                    color = RED[2]
+                elif ageOfCells[row][column] < 90:
+                    color = RED[3]
+                elif ageOfCells[row][column] > 120:
+                    color = RED[4]
+
+
+
             pygame.draw.rect(screen,
                              color,
                              [(MARGIN + WIDTH) * column + MARGIN,
@@ -118,10 +146,8 @@ while not done:
                               WIDTH,
                               HEIGHT])
 
-    pygame.display.flip()            
+
+    pygame.display.flip()
  
 
-
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
 pygame.quit()
